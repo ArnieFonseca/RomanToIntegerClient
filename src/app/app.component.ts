7911c44fc28c45f72e713b0b93705e7d2b47c950
Arnie Fonseca <arniefonseca@hotmail.com>
2024-11-16 13:08:50 -0500

    Added Routing in the Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import  {RomanIntegerComponent} from './roman-integer/roman-integer.component'
 import { NavigationComponent } from './navigation/navigation.component';
 import { IntegerRomanComponent } from  './integer-roman/integer-roman.component'
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RomanIntegerComponent,NavigationComponent, IntegerRomanComponent,],
+  imports: [RouterOutlet,
+    RomanIntegerComponent,
+    NavigationComponent, 
+    IntegerRomanComponent,
+    HomeComponent,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RomanIntegerComponent } from './roman-integer/roman-integer.component';
+import { IntegerRomanComponent } from './integer-roman/integer-roman.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component:HomeComponent},
+    {path: "roman-integer", component:RomanIntegerComponent},
+    {path: "integer-roman", component:IntegerRomanComponent},
+];
