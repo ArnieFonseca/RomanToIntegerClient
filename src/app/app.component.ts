fe1c50be4a7af2b74aac8e811bdb92ce6cc1ebe0
Arnie Fonseca <arniefonseca@hotmail.com>
2024-11-16 14:54:39 -0500

    Move the Rounting to the Navigation Compoment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import  {RomanIntegerComponent} from './roman-integer/roman-integer.component'
-import { NavigationComponent } from './navigation/navigation.component';
-import { IntegerRomanComponent } from  './integer-roman/integer-roman.component'
-import { HomeComponent } from './home/home.component';
+import { Component } from '@angular/core'
+import { NavigationComponent } from './navigation/navigation.component' 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,
-    RomanIntegerComponent,
+  imports: [ 
     NavigationComponent, 
-    IntegerRomanComponent,
-    HomeComponent,
-    RouterLink,
-    RouterLinkActive,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
